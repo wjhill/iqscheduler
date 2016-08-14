da5be483662524784d890ae7322ab12dceeaccd7
Woodrow Hill <cfe1996e@opayq.com>
2016-08-14 13:22:39 -0400

    End date creation works
    - Duration method passes tests
    
    - Had to convert moment object to JS Date to fit w/Jasmine (see https://stackoverflow.com/questions/33380311/jasmine-date-mocking-with-moment-js)

--- __tests__/event-tests.js
+++ __tests__/event-tests.js
@@ -8,5 +8,14 @@ describe('Set the event information', () => {
         var textOfStartDate = "2016-08-20 11:00:00";
         var startClass = event.start(textOfStartDate);
         expect(startClass).toEqual(moment(textOfStartDate));
+    }),
+        it('Ensures the event starting and ending data are created correctly', () => {
+        var moment = require('moment');
+        var event = require('../event');
+        var textOfStartDate = "2016-08-20 11:00:00";
+        var textOfEndDate = "2016-08-20 12:00:00";
+        var startClass = event.start(textOfStartDate);
+        var endClass = event.duration(startClass, "1");
+        expect(endClass.toDate()).toEqual(moment(textOfEndDate).toDate());
     })
 })
